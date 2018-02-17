50555e909e07e2407cb74fc5576b6313210afbec
Ryan Barnard <rbarnard@arane.us>
2018-02-16 19:50:56 -0500

    Added a helper function to extract just the housekeeping gene expressions.

--- R/normalize.R
+++ R/normalize.R
@@ -1,7 +1,14 @@
-substituteZeroExpressions <- function(Expr) {
+substituteZeroExpressions <- function(expr) {
    # TODO: Where does this expression come from?
-   replacementValue <- log(min(Expr[Expr > 0]) / 2, base=2)
-   Expr[Expr == 0] <- replacementValue 
+   replacementValue <- log(min(expr[expr > 0]) / 2, base=2)
+   expr[expr == 0] <- replacementValue 
 
-   Expr
+   expr
+}
+
+getHousekeepingGeneExpressions <- function(expr) {
+   hkg <- getHousekeepingGenes()
+   hkGeneIds <- hkg$probe.id
+
+   expr[match(hkGeneIds, rownames(expr)), ]
 }
--- tests/testthat/test_normalize.R
+++ tests/testthat/test_normalize.R
@@ -13,3 +13,19 @@ test_that("all zeros are replaced", {
 
    expect_true(all(abs(expr) > 1e-12))
 })
+
+
+## Housekeeping Gene Expressions
+
+test_that("housekeeping genes expressions correctly extracted", {
+   load(system.file(file.path('testdata', 'expressions.rda'), package=.PACKAGE_NAME))
+
+   expr <- getHousekeepingGeneExpressions(expr)
+
+   expect_equal(nrow(expr), 4)
+   expect_equal(rownames(expr), c('200801_x_at', '208695_s_at', '212869_x_at', '213583_x_at'))
+
+   ## The following is basically just a checksum in lieu of testing the whole matrix
+   ## or just a portion
+   expect_equal(sum(expr), 345.29)
+})
