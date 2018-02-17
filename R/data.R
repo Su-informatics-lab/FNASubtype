5725edff14982d659e98d6d6f0626433bdedf128
Ryan Barnard <rbarnard@arane.us>
2018-02-16 19:35:45 -0500

    Added the gene annotation data and a helper method to get just the housekeeping genes.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,20 @@
+# TODO: Update the data to name the genes using the identifiers Jing suggested
+
 getCoefficients <- function() {
    filepath <- system.file("extdata", 'coefficients.rda', package=.PACKAGE_NAME)
    varName <- load(filepath)
 
    get(varName)
+}
+
+getAnnotations <- function() {
+   filepath <- system.file("extdata", 'annotations.csv', package=.PACKAGE_NAME)
+
+   read.csv(filepath)
+}
+
+getHousekeepingGenes <- function() {
+   hkg <- getAnnotations()
+
+   hkg[hkg$Gene.Selection == 'HK4/6', ]
 }
--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -22,3 +22,23 @@ test_that("coefficient rows 2:16 are not blank", {
 
    expect_true(all(rownames(coefs)[-1] != ""))
 })
+
+
+# Annotation Data
+
+test_that("annotation data is a 19x5 data frame", {
+   anno <- getAnnotations()
+
+   expect_equal(nrow(anno), 19)
+   expect_equal(ncol(anno), 5)
+   expect_true(is.data.frame(anno))
+})
+
+test_that("get housekeeping genes returns correct records", {
+   hkg <- getHousekeepingGenes()
+
+   expect_equal(nrow(hkg), 4)
+   expect_true(
+      setequal(hkg$probe.id, c('200801_x_at', '208695_s_at', '212869_x_at', '213583_x_at'))
+   )
+})
