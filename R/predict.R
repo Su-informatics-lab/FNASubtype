3cd4934c420e3f7ac906fe178f7635f0dce21ca4
Ryan Barnard <rbarnard@arane.us>
2018-02-19 17:13:25 -0500

    Added the function to generate predictions.

--- R/predict.R
+++ R/predict.R
@@ -9,3 +9,24 @@ buildInputMatrix <- function(expr) {
    # as rows and genes (variables) as columns.
    t(as.matrix(geneExpressions))
 }
+
+generatePredictions <- function(inputMatrix) {
+   coefficients <- getCoefficients()
+   intercept <- coefficients[1]
+   genes <- coefficients[-1]
+
+   # Multiply the input matrix by the gene coefficients
+   prediction <- intercept + (inputMatrix %*% genes)
+
+   # And convert to a probability
+   prediction <- exp(prediction) / (1 + exp(prediction))
+
+   # Turn the predicted probabilities into a data frame with both the
+   # probability and the predicted cancer type.
+   prediction <- data.frame(
+      probability=prediction,
+      type=factor(ifelse(prediction > 0.5, 'A', 'S'))
+   )
+
+   prediction
+}
--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -24,3 +24,45 @@ test_that("build input matrix returns only genes of interest", {
 
    expect_equal(actual, expected)
 })
+
+
+# Predicted Outcomes
+
+test_that("generate predictions probability column in [0, 1]", {
+   load(system.file(file.path('testdata', 'inputMatrix.rda'), package=.PACKAGE_NAME))
+
+   prediction <- generatePredictions(inputMatrix)
+
+   expect_true(all(prediction$probability >= 0))
+   expect_true(all(prediction$probability <= 1))
+})
+
+test_that("generate predictions type column is A when probability > 0.5", {
+   load(system.file(file.path('testdata', 'inputMatrix.rda'), package=.PACKAGE_NAME))
+
+   prediction <- generatePredictions(inputMatrix)
+
+   prediction <- prediction[prediction$prob > 0.5,]
+
+   expect_true(all(prediction$type == 'A'))
+})
+
+test_that("generate predictions type column is S when probability <= 0.5", {
+   load(system.file(file.path('testdata', 'inputMatrix.rda'), package=.PACKAGE_NAME))
+
+   prediction <- generatePredictions(inputMatrix)
+
+   prediction <- prediction[prediction$prob <= 0.5,]
+
+   expect_true(all(prediction$type == 'S'))
+})
+
+test_that("generate predictions retains subject rownames", {
+   load(system.file(file.path('testdata', 'inputMatrix.rda'), package=.PACKAGE_NAME))
+
+   expected <- rownames(inputMatrix)
+
+   prediction <- generatePredictions(inputMatrix)
+
+   expect_equal(rownames(prediction), expected)
+})
